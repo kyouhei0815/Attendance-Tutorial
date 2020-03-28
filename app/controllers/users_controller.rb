aeffdef0299678042ec0e887afe9a079a7586b16
kyouhei0815 <0hu6y5573231t7p@au.com>
2020-03-28 12:48:31 +0000

    Finish add-basic-info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -54,7 +54,7 @@ class UsersController < ApplicationController
     flash[:danger] = "#{@user.name}の更新は失敗しました。<br>" + @user.errors.full_messages.join("<br>")
   end
   redirect_to users_url
-end
+ end
  
 
   private
